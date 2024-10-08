5ba4a041f8f3be9115dd306343b214ed248c7b40
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-10-08 19:58:37 -0400

    loader and initial version of schema classes;

--- examples/example.py
+++ examples/example.py
@@ -1,4 +1,17 @@
+from osvutils.core.loader import OSVDataLoader
 
-from osvutils import main
 
-main.main()
+loader = OSVDataLoader()
+
+osv_is_cve_id_count = 0
+
+loader(ecosystems=['GIT'])
+
+for _, vals in loader:
+    for _, record in vals.items():
+        if record.is_cve_id():
+            osv_is_cve_id_count += 1
+        elif record.has_cve_id():
+            osv_is_cve_id_count += 1
+
+print(f"CVE IDs: {osv_is_cve_id_count}")
